2aefad312567cf24893e4ed01058365768314775
EzequielSB1 <90113082+EzequielSB1@users.noreply.github.com>
2022-05-21 12:48:37 -0300

    firebase 2

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -3,39 +3,64 @@ import { useParams } from 'react-router-dom'
 import ItemDetail from './ItemDetail'
 import productosIniciales from './productos.json'
 import { db } from './firebase'
+import { collection, addDoc, getDoc, doc, getDocs, query, where } from 'firebase/firestore'
+
 
 const ItemDetailContainer = () => {
     let [cargando,setCargando] = useState(true)
     let [producto,setProducto] = useState({}) 
     let {id} = useParams()
   
-    useEffect(()=>{
 
-      console.log({id})
+
+    useEffect(() => {
+
+
   
-      const resultado = productosIniciales.filter((producto)=>{
-        return producto.id == id
-      })[0]
+      const productosCollection = collection( db, "productos" )
+      const resultadoDelDoc = doc(productosCollection, id)
+      const consulta = getDoc(resultadoDelDoc)
+  
+      consulta.then((resultado)=>{
 
-      console.log(resultado)
-
-      const pedido = new Promise((res)=>{
-        setTimeout(()=>{
-          res(resultado)
-        }, 300)
-      })
-      
-      pedido.then(()=>{
-        console.log("Termino de cargar bien")
+        setProducto(resultado.data())
         setCargando(false)
-        setProducto(resultado)
-      })
-
-      pedido.catch(()=>{
-        console.log("Carga fallida, recargue la pagina")
-      })
   
-    },[id])
+        })
+        consulta.catch((error)=>{
+          console.log(error)
+
+        })
+        consulta.finally(()=>{
+  
+        })
+    // useEffect(()=>{
+
+    //   console.log({id})
+  
+    //   const resultado = productosIniciales.filter((producto)=>{
+    //     return producto.id == id
+    //   })[0]
+
+    //   console.log(resultado)
+
+    //   const pedido = new Promise((res)=>{
+    //     setTimeout(()=>{
+    //       res(resultado)
+    //     }, 300)
+    //   })
+      
+    //   pedido.then(()=>{
+    //     console.log("Termino de cargar bien")
+    //     setCargando(false)
+    //     setProducto(resultado)
+    //   })
+
+    //   pedido.catch(()=>{
+    //     console.log("Carga fallida, recargue la pagina")
+    //   })
+  
+    // },[id])
 
 
     // if (cargando) {
@@ -54,9 +79,13 @@ const ItemDetailContainer = () => {
     //   )
     // }
 
+    setCargando(false)
+
+      })
+
     return (
       <>
-        {cargando ? <div className='container__carga'><div className='carga'></div></div> : <section className='details'><ItemDetail key={productosIniciales.id} producto={producto} imagen={productosIniciales.imagen} nombre={productosIniciales.nombre} descripcion={productosIniciales.descripcion} counter={productosIniciales.counter} stock={productosIniciales.stock}/></section>}
+        {cargando ? <div className='container__carga'><div className='carga'></div></div> : <section className='details'><ItemDetail key={producto.id} producto={producto} imagen={producto.imagen} nombre={producto.title} descripcion={producto.descripcion} counter={producto.counter} stock={producto.stock}/></section>}
       </>
     )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,38 +4,85 @@ import { useParams } from 'react-router-dom'
 import ItemList from './ItemList'
 import productosIniciales from './productos.json'
 import { db } from './firebase'
-import { collection, addDoc, getDoc, doc, getDocs } from 'firebase/firestore'
+import { collection, addDoc, getDoc, doc, getDocs, query, where } from 'firebase/firestore'
 
 // component
 const ItemListContainer = () => {
 
   let [cargando,setCargando] = useState(true)
   let [productos,setProductos] = useState([]) 
-  let {nombreCategoria} = useParams()
+  let {nombreCategoria, test} = useParams()
 
   useEffect(()=>{
 
+    setCargando(true)
+
     const productosCollection = collection( db, "productos" )
-    const consulta = getDocs(productosCollection)
+    // const consulta = getDocs(queryFiltros)
+
+
+    if (nombreCategoria !== undefined) {
+
+      
+      const queryFiltros = query(productosCollection,where("categoria", "==", nombreCategoria))
+
+      const consulta = getDocs(queryFiltros)
 
     consulta.then((resultado)=>{
-        resultado.docs.map(doc => {
+        const productos = resultado.docs.map(doc => {
           const productosConId = doc.data()
           productosConId.id = doc.id
 
-          console.log(productosConId)
+          return productosConId
         })
+
+        // console.log(productos)
+        setProductos(productos)
+        setCargando(false)
+
       })
 
-      setProductos(productos)
-      setCargando(false)
+      
 
       consulta.catch((error)=>{
+
+        console.log(error)
 
       })
       consulta.finally(()=>{
 
       })
+
+    } else {
+
+      const consulta = getDocs(productosCollection)
+
+      consulta.then((resultado)=>{
+          const productos = resultado.docs.map(doc => {
+            const productosConId = doc.data()
+            productosConId.id = doc.id
+
+            return productosConId
+          })
+
+          // console.log(productos)
+          setProductos(productos)
+          setCargando(false)
+
+        })
+
+      
+
+      consulta.catch((error)=>{
+
+        console.log(error)
+
+      })
+      consulta.finally(()=>{
+
+      })
+
+    }
 
   //   if (nombreCategoria === undefined) {
 
@@ -52,7 +99,7 @@ const ItemListContainer = () => {
   //     })
 
   //     pedido.catch(()=>{
-  //       console.log("Carga fallida, recargue la pagina")
+  //       
   //     })
 
   //   } else {
@@ -94,13 +141,19 @@ const ItemListContainer = () => {
   //   )
   // })
 
-  return (
-    <section className='products'>  
-      <ItemList productos={productos}/> 
-    </section>)
-  
-  
+  console.log(productos)
+  setCargando(false)
+
 })
+
+  return (
+    <>
+      {cargando ? <div className='container__carga'><div className='carga'></div></div> : <section className='products'><ItemList productos={productos}/> </section>}
+    </>
+  )
+  
+  
+
 }
 
 // exports
